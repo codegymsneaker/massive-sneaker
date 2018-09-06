ba59334fb9bd3f70b2a1dfdac1fb832d38c852ab
Minh Lee <minhlee.fre.mta@gmail.com>
2018-09-06 14:20:51 +0700

    config web security

---
+++ src/main/java/com/codegym/sneaker/configuration/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.codegym.sneaker.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    CustomSuccessHandler customSuccessHandler;
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("root123").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/admin").access("hasRole('ADMIN')")
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login")
+                .usernameParameter("username").passwordParameter("password")
+                .and().csrf()
+                .and().exceptionHandling().accessDeniedPage("/Access_Denied")
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+        http.exceptionHandling().accessDeniedPage("/403");
+    }
+}
