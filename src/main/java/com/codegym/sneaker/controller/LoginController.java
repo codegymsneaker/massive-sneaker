f818c036e8d6f72d1bbdba97814ead09a0f05671
Minh Lee <minhlee.fre.mta@gmail.com>
2018-09-11 12:44:53 +0700

    change stylesheet and display error notification

--- src/main/java/com/codegym/sneaker/controller/LoginController.java
+++ src/main/java/com/codegym/sneaker/controller/LoginController.java
@@ -1,13 +1,20 @@
 package com.codegym.sneaker.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -39,6 +46,24 @@ public class LoginController {
     public String adminPage(ModelMap model) {
         model.addAttribute("user", getPrincipal());
         return "redirect:/product";
+    }
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(
+            @RequestParam(value = "error", required = false) String error,
+            @RequestParam(value = "logout", required = false) String logout) {
+
+        ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("error", "Invalid username and password!");
+        }
+
+        if (logout != null) {
+            model.addObject("msg", "You've been logged out successfully.");
+        }
+        model.setViewName("login");
+
+        return model;
+
     }
 
     @RequestMapping(value = "/Access_Denied", method = RequestMethod.GET)
