2009108e8cadba0f748ca6995d58f10b70f39e7d
Minh Lee <minhlee.fre.mta@gmail.com>
2018-09-17 10:29:37 +0700

    edit role name

--- src/main/java/com/codegym/sneaker/configuration/CustomSuccessHandler.java
+++ src/main/java/com/codegym/sneaker/configuration/CustomSuccessHandler.java
@@ -7,7 +7,6 @@ import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,21 +21,24 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
     @Override
-    protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         String targetUrl = determineTargetUrl(authentication);
-        String email = request.getParameter("email");
+        String email =request.getParameter("email");
+
         if (response.isCommitted()) {
             System.out.println("Can't redirect");
             return;
         }
+
         Cookie cookie = new Cookie("setUser",email);
         response.addCookie(cookie);
         cookie.setMaxAge(24*60*60);
+
         redirectStrategy.sendRedirect(request, response, targetUrl);
     }
 
-    protected String determineTargetUrl(Authentication authentication) {
-        String url = "";
+    private String determineTargetUrl(Authentication authentication) {
+        String url;
 
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
 
@@ -51,24 +53,18 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         } else if (isUser(roles)) {
             url = "/user";
         } else {
-            url = "/accessDenied";
+            url = "/access_denied";
         }
         return url;
     }
 
     private boolean isUser(List<String> roles) {
-        if (roles.contains("USER")) {
-            return true;
-        }
-        return false;
+        return roles.contains("ROLE_USER");
     }
 
 
     private boolean isAdmin(List<String> roles) {
-        if (roles.contains("ADMIN")) {
-            return true;
-        }
-        return false;
+        return roles.contains("ROLE_ADMIN");
     }
 
     @Override
